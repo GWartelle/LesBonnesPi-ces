77f11041767007c3667cfca6bf53d19d2f74699a
GWartelle <115559211+GWartelle@users.noreply.github.com>
2024-01-27 23:56:39 +0100

    Mise à jour avis grâce à l'API

--- avis.js
+++ avis.js
@@ -4,7 +4,15 @@ export function ajoutListenersAvis() {
     for (let i = 0; i < piecesElements.length; i++) {
         piecesElements[i].addEventListener("click", async function (event) {
             const id = event.target.dataset.id;
-            fetch(`http://localhost:8081/pieces/${id}/avis`);
+            const reponse = await fetch(`http://localhost:8081/pieces/${id}/avis`);
+            const avis = await reponse.json();
+            const pieceElement = event.target.parentElement;
+
+            const avisElement = document.createElement("p")
+            for (let j = 0; j < avis.length; j++) {
+                avisElement.innerHTML += `<b>${avis[j].utilisateur}:</b> ${avis[j].commentaire} <br>`;
+            }
+            pieceElement.appendChild(avisElement);
         });
     }
 }
